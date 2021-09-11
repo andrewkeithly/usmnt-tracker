9c02788779665021ed9ca1da58ce0fc623928785
Andrew Keithly <andrew.keithly@gmail.com>
2021-09-10 20:58:47 -0500

    Started with Grommet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Grommet, Grid, Box } from "grommet";
 
 function App() {
   return (
-    <div className="App">
+    <Grommet plain>
       <header className="App-header"></header>
-    </div>
+      <Grid
+        rows={["xxsmall", "xsmall"]}
+        columns={["xsmall", "small"]}
+        gap="small"
+        areas={[
+          { name: "header", start: [0, 0], end: [1, 0] },
+          { name: "nav", start: [0, 1], end: [0, 1] },
+          { name: "main", start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Box gridArea="header" background="brand" />
+        <Box gridArea="nav" background="light-5" />
+        <Box gridArea="main" background="light-2" />
+      </Grid>
+    </Grommet>
   );
 }
 
